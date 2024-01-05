f67c6c8e829e4bf7480d4e801fafff4caabbd86d
jaemoon <woans5970@naver.com>
2024-01-05 14:36:59 +0900

    section 2: 체크박스 - 멀티

--- src/main/java/hello/itemservice/web/form/FormItemController.java
+++ src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -1,4 +1,4 @@
-package hello.itemservice.web.basic;
+package hello.itemservice.web.form;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
@@ -10,15 +10,27 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Controller
-@RequestMapping("/basic/items")
+@RequestMapping("/form/items")
 @RequiredArgsConstructor // final에 관한 생성자 자동 생성
-public class BasicItemController {
+public class FormItemController {
 
     private final ItemRepository itemRepository;
+
+    @ModelAttribute("regions")
+    public Map<String, String> regions() {
+        Map<String, String> regions = new LinkedHashMap<>();
+        regions.put("SEOUL", "서울");
+        regions.put("DAGUE", "대구");
+        regions.put("JEJU", "제주");
+
+        return regions;
+    }
 
 //    @Autowired //생성자가 하나일 경우 생략가능
 //    public BasicItemController(ItemRepository itemRepository) {
@@ -29,20 +41,20 @@ public class BasicItemController {
     public String items(Model model) {
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
-        return "basic/items";
+        return "form/items";
     }
 
     @GetMapping("/{itemId}")
     public String item(@PathVariable long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
-        return "basic/item";
+        return "form/item";
     }
 
     @GetMapping("/add")
     public String addForm(Model model) {
         model.addAttribute("item", new Item());
-        return "basic/addForm";
+        return "form/addForm";
     }
 
 //    @PostMapping("/add")
@@ -59,7 +71,7 @@ public class BasicItemController {
 
         model.addAttribute("item", item);
 
-        return "basic/addForm";
+        return "form/addForm";
     }
 
 //    @PostMapping("/add")
@@ -68,7 +80,7 @@ public class BasicItemController {
 
         model.addAttribute("item", item); // 자동 추가 -> 생략 가능
 
-        return "basic/addForm";
+        return "form/addForm";
     }
 
 //    @PostMapping("/add")
@@ -78,7 +90,7 @@ public class BasicItemController {
          */
         itemRepository.save(item);
 
-        return "basic/addForm";
+        return "form/addForm";
     }
 
 //    @PostMapping("/add")
@@ -88,37 +100,39 @@ public class BasicItemController {
          */
         itemRepository.save(item);
 
-        return "basic/item";
+        return "form/item";
     }
 //    @PostMapping("/add")
     public String addItemV5(Item item) {
         itemRepository.save(item);
 
-        return "redirect:/basic/items/" + item.getId();
+        return "redirect:/form/items/" + item.getId();
     }
 
     @PostMapping("/add")
     public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
         log.info("item.open={}", item.getOpen());
+        log.info("item.regions={}", item.getRegions());
+
         Item savedItem = itemRepository.save(item);
 
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
 
-        return "redirect:/basic/items/{itemId}";
+        return "redirect:/form/items/{itemId}";
     }
 
     @GetMapping("/{itemId}/edit")
     public String editForm(@PathVariable Long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
-        return "basic/editForm";
+        return "form/editForm";
     }
 
     @PostMapping("/{itemId}/edit")
     public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
         itemRepository.update(itemId, item);
-        return "redirect:/basic/items/{itemId}";
+        return "redirect:/form/items/{itemId}";
     }
 
     /*
